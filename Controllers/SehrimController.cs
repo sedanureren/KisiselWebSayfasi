2b3fe7de186f3517eebce9c0a958cc81bd8175e5
sedanureren <77543691+sedanureren@users.noreply.github.com>
2022-12-13 23:09:59 +0300

    6.commit

---
+++ Controllers/SehrimController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KisiselWebSayfasi.Controllers
+{
+    public class SehrimController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
