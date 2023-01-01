67ec4f537c058367b28fb2a8775f4de2101b4eb1
sedanureren <77543691+sedanureren@users.noreply.github.com>
2023-01-01 23:16:50 +0300

    12.commit

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Identity;
 using System.Configuration;
 using Microsoft.EntityFrameworkCore;
 using KisiselWebSayfasi.Areas.Identity.Data;
+using KisiselWebSayfasi.Repositories;
+using KisiselWebSayfasi.Core.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
@@ -12,9 +14,14 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
 builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllersWithViews();
+#region Authorization
+AddAuthorizationPolicies(builder.Services);
+#endregion
+AddScoped();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -33,3 +40,23 @@ app.MapControllerRoute(
     pattern: "{controller=AnaSayfa}/{action=Index}/{id?}");
 app.MapRazorPages();
 app.Run();
+void AddAuthorizationPolicies(IServiceCollection services)
+{
+    services.AddAuthorization(options =>
+    {
+        options.AddPolicy("EmployeeOnly",
+        policy => policy.RequireClaim("EmployeeNumber"));
+    });
+    services.AddAuthorization(options =>
+    {
+        options.AddPolicy(Constants.Policies.RequireAdmin, policy=>policy.RequireRole(Constants.Roles.Administrator));
+        options.AddPolicy(Constants.Policies.RequireManager, policy => policy.RequireClaim(Constants.Roles.Manager));
+    });
+}
+void AddScoped()
+{
+    builder.Services.AddScoped<IUserRepository, UserRepository>();
+    builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+    builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+
+}
